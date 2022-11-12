baeb12e5f41f3e26b4673d5933741d0f27a7c62d
PriscilaButzke <priscilabutzke@outlook.com>
2022-11-11 22:13:04 -0300

    :construction: conclusão crud

--- conexao.php
+++ conexao.php
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = NULL;
     $celular = NULL;
 }
+//conecta ao banco de dados
 try {
     $conexao = new PDO("mysql:host=localhost; dbname=crudsimples", "root", "");
 
@@ -26,7 +27,16 @@ try {
 //ação “act” para salvar “save”, se sim, então entra em um novo bloco try e catch.
 if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
     try {
-        $stmt = $conexao->prepare("INSERT INTO contatos (nome, email, celular) VALUES (?, ?, ?)");
+        //Update
+        // se a variável $id estiver vazia, será feito um Insert
+        // se for diferente de vazio, sera feito um update
+        if ($id != "") {
+            $stmt = $conexao->prepare("UPDATE contatos SET nome=?, email=?, celular=? WHERE id = ?");
+            $stmt->bindParam(4, $id);
+        } else {
+            $stmt = $conexao->prepare("INSERT INTO contatos (nome, email, celular) VALUES (?, ?, ?)");
+        }
+
         $stmt->bindParam(1, $nome);
         $stmt->bindParam(2, $email);
         $stmt->bindParam(3, $celular);
@@ -48,7 +58,43 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
         echo "Erro: " . $erro->getMessage();
     }
 }
-//read
+
+//recupera os dados
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM contatos WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $nome = $rs->nome;
+            $email = $rs->email;
+            $celular = $rs->celular;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: " . $erro->getMessage();
+    }
+}
+//delete
+
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
+
+    try {
+        $stmt = $conexao->prepare("DELETE FROM contatos WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+    
+        if ($stmt->execute()) {
+            echo "Registro foi excluído com êxito";
+            $id = null;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
 
 ?>
 
@@ -68,15 +114,24 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
         <hr>
         <div class="d-flex justify-content-start ms-5">
 
+            <div>
+                <input class="form-control" type="hidden" name="id" <?php
+                                                                    // Preenche o id no campo id com um valor "value"
+                                                                    if (isset($id) && $id != null || $id != "") {
+                                                                        echo "value=\"{$id}\"";
+                                                                    }
+                                                                    ?> />
 
-            <div  class="mb-3 row">
+            </div>
+
+            <div class="mb-3 row">
                 <label for="inputEmail4" class="form-label">Nome</label>
-                <input class="form-control" style="width:300px ;" type="text" name="id" <?php
-                                                                                        // Preenche o id no campo id com um valor "value"
-                                                                                        if (isset($id) && $id != null || $id != "") {
-                                                                                            echo "value=\"{$id}\"";
-                                                                                        }
-                                                                                        ?> />
+                <input class="form-control" style="width:300px ;" type="text" name="nome" <?php
+                                                                                            // Preenche o id no campo id com um valor "value"
+                                                                                            if (isset($nome) && $nome != null || $nome != "") {
+                                                                                                echo "value=\"{$nome}\"";
+                                                                                            }
+                                                                                            ?> />
             </div>
 
             <div class="mb-3 row">
@@ -101,11 +156,11 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
 
             </div>
             <div class="mt-4">
-            <input type="submit" value="salvar" class="btn btn-primary" />
-            <input type="reset" value="Novo" class="btn btn-primary" />
+                <input type="submit" value="salvar" class="btn btn-primary" />
+                <input type="reset" value="Novo" class="btn btn-primary" />
+            </div>
         </div>
-        </div>
-        
+
         <hr>
     </form>
 
@@ -116,7 +171,36 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
             <th>Celular</th>
             <th>Ações</th>
         </tr>
+
+
+        <?php
+        // Bloco que realiza o papel do Read - recupera os dados e apresenta na tela
+        try {
+            $stmt = $conexao->prepare("SELECT * FROM contatos");
+
+            if ($stmt->execute()) {
+                while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                    echo "<tr>";
+                    echo "<td>" . $rs->nome . "</td><td>" . $rs->email . "</td><td>" . $rs->celular
+                        . "</td><td><center><a href=\"?act=upd&id=" . $rs->id . "\">[Alterar]</a>"
+                        . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+
+                        . "<a class='btn btn-primary' href=\"?act=del&id=" . $rs->id . "\">[Excluir]</a></center></td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "Erro: Não foi possível recuperar os dados do banco de dados";
+            }
+        } catch (PDOException $erro) {
+            echo "Erro: " . $erro->getMessage();
+        }
+        ?>
+
+
+
     </table>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
